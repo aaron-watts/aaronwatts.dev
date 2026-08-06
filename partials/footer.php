973f8d7fdc72c259e684b77e50a5d0f4ff8903b2
Aaron Watts <hello@aaronwatts.dev>
2026-08-06 09:08:57 +0100

    create footer and rss-icon partials

---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <div>Made by Hand. Powered by</div>
+  <a href="https://pages.github.com/" target="_blank">
+    <img alt="https://pages.github.com/" src="/assets/svg/githubpages.svg"/>
+  </a>
+</footer>
---
+++ partials/rss.php
@@ -0,0 +1,8 @@
+<div class="svg-container">
+  <a href="/rss">
+  <svg class="rss">
+    <use href="/assets/svg/svg-map.svg#rss-feed" />
+  </svg>
+  </a>
+</div>
+
