99b16cac7e173e21bd3e7fcb4ce54992b2534cd7
Aaron Watts <hello@aaronwatts.dev>
2026-08-07 17:02:15 +0100

    shorten blog descriptions

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-$blogDesc = fn (): string => "Here I talk about things that don't fit in with the topics of my other blog reels. Topics will likely fall into the realm of nerdery, with hobbyist programming and web development,  tech gadgets, and maybe video games and books.";
-$guidesDesc = fn (): string => "Some guides on things that I have worked on where resources are either hard to find or scattered. You might save yourself some time or learn something new if you're working on anything that comes up in this section.";
+$blogDesc = fn (): string => "Topics will likely fall into the realm of nerdery, with hobbyist programming and web development,  tech gadgets, and maybe video games and books.";
+$guidesDesc = fn (): string => "Some guides on things that I have worked on where resources are either hard to find or scattered.";
 $techDesc = fn (): string => "Technology that I consider to be <em>thoughtful</em>, where there are more meaningful priorities than exponential growth and high profit margins.";
 
 return array(
