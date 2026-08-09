b4fb212607821ec40b9b5ee62ce96c078b0df9c5
Aaron Watts <hello@aaronwatts.dev>
2026-08-09 14:49:20 +0100

    home > nav external

--- views/home.php
+++ views/home.php
@@ -95,7 +95,7 @@
   </head>
   <body>
     <header>
-      <nav aria-label="External Links">
+      <nav aria-label="Complimentary">
 <?php require PARTIALS_PATH . 'rss.php'; ?>
         <a href="https://sites.aaronwatts.dev">More by aaronwatts@dev</a>
       </nav>
