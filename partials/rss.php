af0d08f9852d6bae8d9af9c057de50560bf144bf
Aaron Watts <hello@aaronwatts.dev>
2026-08-10 08:19:35 +0100

    build chcanges

--- partials/rss.php
+++ partials/rss.php
@@ -1,6 +1,6 @@
 <div class="svg-container">
   <a href="/rss">
-    <span class="screen-reader-text" style="position: absolute; left: -9999px;">RSS Feeds</span>
+    <span class="screen-reader-text">RSS Feeds</span>
     <svg class="rss">
       <use href="/assets/svg/svg-map.svg#rss-feed" />
     </svg>
