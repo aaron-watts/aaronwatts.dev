1aeabc69da6a35ca61fd68486b797972474421d4
Aaron Watts <hello@aaronwatts.dev>
2026-08-19 11:33:11 +0100

    create search form partial

---
+++ partials/search.php
@@ -0,0 +1,7 @@
+<search id="search" class="noscript">
+  <form action="/search">
+    <input type="text" name="query" required placeholder="Search posts...">
+    <button type="submit">Search</button>
+  </form>
+</search>
+
