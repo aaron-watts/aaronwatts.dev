166bc5193cb1131eafe7e2beda847e7be6214688
Aaron Watts <hello@aaronwatts.dev>
2026-09-19 15:54:57 +0100

    build changes

---
+++ dist/assets/scripts/copytext.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const noscript = document.querySelectorAll('.noscript');
+  noscript.forEach(i => i.classList.remove('noscript'));
+
+  const copyBtns = document.querySelectorAll('button[data-copytext]');
+
+  copyBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const copyText = btn.dataset.copytext;
+      
+      navigator.clipboard.writeText(copyText);
+      
+      btn.textContent = "Link Copied!";
+      btn.ariaLive = 'off';
+      btn.blur();
+      
+      setTimeout(function() {
+        btn.textContent = "Copy Link";
+        btn.ariaLive = 'assertive';
+      }, 3000);
+    });
+  });
+}
